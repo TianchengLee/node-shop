d7c85cf4371b4fb3d41df2c0ad6a5c81da3d683c
TianchengLee <ltc6634284@gmail.com>
2018-12-13 23:33:13 +0800

    添加收货人完成

--- controller/users.js
+++ controller/users.js
@@ -27,23 +27,32 @@ const getReceiverAddressSql = `SELECT id, receiver_name, mobile, postcode, provi
 const deleteReceiverAddressSql = `UPDATE receiver_address
                                   SET del_state = 1
                                   WHERE id = ?`
+const addReceiverAddressSql = `INSERT INTO receiver_address SET ?`
 
 module.exports = {
   registerAction(req, res) {
-    if (!req.session.vCode || !req.body.vCode || req.session.vCode.toLowerCase() != req.body.vCode.toLowerCase()) return res.sendErr(400, '验证码错误!')
-    if (!req.body.username || !req.body.username.trim()) return res.sendErr(400, '用户名未填写!')
-    if (!req.body.password || !req.body.password.trim()) return res.sendErr(400, '密码未填写!')
-    if (!req.body.mobile || !req.body.mobile.trim()) return res.sendErr(400, '用户手机号未填写!')
+
+    let attrs = ['username', 'password', 'vCode', 'mobile']
+
+    if (!checkFormBody(req, res, attrs)) return
+
+    if (!req.session.vCode || req.session.vCode.toLowerCase() != req.body.vCode.toLowerCase()) return res.sendErr(400, '验证码错误!')
 
     req.body.password = bcrypt.hashSync(req.body.password, 10)
 
-    let userInfo = { ...req.body, ctime: moment().format('YYYY-MM-DD HH:mm:ss') }
+    let userInfo = { ctime: moment().format('YYYY-MM-DD HH:mm:ss') }
+
+    attrs.forEach(attr => {
+      userInfo[attr] = req.body[attr]
+    })
 
     delete userInfo.vCode
 
     sqlExcute(getUserCountSql, userInfo.username)
       .then(result => {
-        if (result[0].count > 0) return res.sendErr(400, '用户名已存在!')
+        if (result[0].count > 0) {
+          throw new Error('用户名已存在!')
+        }
         return sqlExcute(addUserSql, userInfo)
       })
       .then(result => {
@@ -62,13 +71,14 @@ module.exports = {
           userInfo.token = token
           return sqlExcute(addUserTokenSql, tokenInfo)
         }
-      }, err => {
-        res.sendErr(500, err.message)
       })
       .then(result => {
         if (result.affectedRows) {
           res.sendSucc('注册成功!', userInfo)
         }
+      })
+      .catch(e => {
+        res.sendErr(400, e.message)
       })
 
   },
@@ -86,17 +96,25 @@ module.exports = {
     res.send(vCode.data)
   },
   checkUsernameAction(req, res) {
-    if (!req.params.username || !req.params.username.trim()) return res.sendErr(400, '用户名未填写!')
+
+    let attrs = ['username']
+
+    if (!checkFormBody(req, res, attrs)) return
 
     sqlExcute(getUserCountSql, req.params.username)
       .then(result => {
-        if (result[0].count > 0) res.sendErr(400, '用户名已存在!')
-        else res.sendSucc('用户名可用!')
+        if (result[0].count > 0) throw new Error('用户名已存在!')
+        res.sendSucc('用户名可用!')
+      })
+      .catch(e => {
+        res.sendErr(400, e.message)
       })
   },
   loginAction(req, res) {
-    if (!req.body.username || !req.body.username.trim()) return res.sendErr(400, '用户名未填写!')
-    if (!req.body.password || !req.body.password.trim()) return res.sendErr(400, '密码未填写!')
+
+    let attrs = ['username', 'password']
+
+    if (!checkFormBody(req, res, attrs)) return
 
     const userInfo = {
       username: req.body.username,
@@ -105,7 +123,7 @@ module.exports = {
 
     sqlExcute(getUserInfoSql, userInfo.username)
       .then(result => {
-        if (!result || result.length === 0) return res.sendErr(400, '用户名不存在!')
+        if (!result || result.length === 0) throw new Error('用户名不存在!')
         const hash = result[0].password
         userInfo.id = result[0].id
         userInfo.mobile = result[0].mobile
@@ -125,16 +143,22 @@ module.exports = {
           delete userInfo.password
           res.sendSucc('登录成功!', userInfo)
         } else {
-          res.sendErr(400, '登录失败!密码错误!')
+          throw new Error('登录失败!密码错误!')
         }
       })
+      .catch(e => {
+        res.sendErr(400, e.message)
+      })
   },
-  
+
   getUserInfoAction(req, res) {
     res.sendSucc('获取用户信息成功!', req.userInfo)
   },
   updateUserInfoAction(req, res) {
-    if (!req.body.mobile || !req.body.mobile.trim()) return res.sendErr(400, '用户手机号未填写!')
+
+    let attrs = ['mobile']
+
+    if (!checkFormBody(req, res, attrs)) return
 
     let userInfo = { mobile: req.body.mobile }
 
@@ -144,31 +168,35 @@ module.exports = {
           req.userInfo.mobile = req.body.mobile
           res.sendSucc('用户信息修改成功!', req.userInfo)
         }
-      }, err => {
-        res.sendErr(500, '用户信息修改失败!')
+      })
+      .catch(e => {
+        res.sendErr(400, e.message)
       })
   },
   updatePasswordAction(req, res) {
-    if (!req.body.oldPassword || !req.body.oldPassword.trim()) return res.sendErr(400, '旧密码未填写!')
-    if (!req.body.newPassword || !req.body.newPassword.trim()) return res.sendErr(400, '新密码未填写!')
+
+    let attrs = ['oldPassword', 'newPassword']
+
+    if (!checkFormBody(req, res, attrs)) return
 
     sqlExcute(getUserInfoSql, req.userInfo.username)
       .then(result => {
-        if (!result || result.length === 0) return res.sendErr(400, '用户名不存在!')
+        if (!result || result.length === 0) throw new Error('用户名不存在!')
         const hash = result[0].password
         if (bcrypt.compareSync(req.body.oldPassword, hash)) {
           let password = bcrypt.hashSync(req.body.newPassword, 10)
           return sqlExcute(updateUserInfoSql, [{ password }, req.userInfo.id])
         } else {
-          res.sendErr(400, '修改密码失败!密码错误!')
+          throw new Error('修改密码失败!密码错误!')
         }
       })
       .then(result => {
         if (result.affectedRows) {
           res.sendSucc('修改密码成功!')
         }
-      }, err => {
-        res.sendErr(500, '修改密码失败!内部错误:' + err.message)
+      })
+      .catch(e => {
+        res.sendErr(400, e.message)
       })
   },
   getReceiverAddressAction(req, res) {
@@ -182,11 +210,54 @@ module.exports = {
     sqlExcute(deleteReceiverAddressSql, receiverId)
       .then(result => {
         if (result.affectedRows) res.sendSucc('删除收货人成功!')
-        else res.sendErr(400, '删除收货人失败!请检查id是否正确!')
+        else throw new Error('删除收货人失败!请检查id是否正确!')
+      })
+      .catch(e => {
+        res.sendErr(400, e.message)
       })
   },
   addReceiverAddressAction(req, res) {
-    console.log(req.body)
-    res.send('ok');
+    let attrs = ['receiver_name', 'mobile', 'postcode', 'province', 'city', 'area', 'detailed_address']
+    if (!checkFormBody(req, res, attrs)) return
+    let receiverInfo = {}
+    attrs.forEach(attr => {
+      receiverInfo[attr] = req.body[attr]
+    })
+    receiverInfo.user_id = req.userInfo.id
+    sqlExcute(addReceiverAddressSql, receiverInfo)
+      .then(result => {
+        if (result.affectedRows) res.sendSucc('添加收货人成功!', receiverInfo)
+        else throw new Error('添加收货人失败!')
+      })
+      .catch(e => {
+        res.sendErr(400, e.message)
+      })
   }
+}
+
+function checkFormBody(req, res, attrs = []) {
+  let pass = true
+  let results = []
+
+  attrs.forEach(item => {
+    if (!(item in req.body) && !(item in req.params) && !(item in req.query)) {
+      results.push(item)
+      pass = false
+    }
+  })
+
+  let checkBodies = [req.body, req.params, req.query]
+
+  checkBodies.forEach(item => {
+    for (let k in item) {
+      let val = item[k]
+      if (!val || !val.toString().trim()) {
+        results.push(k)
+        pass = false
+      }
+    }
+  })
+
+  if (!pass) res.sendErr(400, results.join(',') + '未填写!')
+  return pass
 }
