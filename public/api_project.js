9013a4be919d25edadf654274b0fd9de245ad5a6
TianchengLee <ltc6634284@gmail.com>
2018-12-21 16:08:30 +0800

    APIDOC线上文档更新

--- public/api_project.js
+++ public/api_project.js
@@ -1,6 +1,6 @@
 define({
   "title": "node-shop接口文档",
-  "url": "http://localhost:9999",
+  "url": "http://litc.pro:9999",
   "name": "node-shop",
   "version": "1.0.0",
   "description": "",
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-12-21T08:07:10.612Z",
+    "time": "2018-12-21T08:08:08.145Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
